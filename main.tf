81b6cec3b42e1c2d1b7584946c93107b12c6bbd9
avanpatel25 <avanpatel25@gmail.com>
2023-10-03 22:33:43 +0530

    Updated main.tf
    commited storage file

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-001122-22"
-  location = "europe-west2"
-}
+
 
 resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-random-00112323"
