caa7b0f7b67d7e6dab13b7e1c9d2a2c733e748e8
ExploreDevOps <88178696+ExploreDevOps@users.noreply.github.com>
2024-02-04 15:58:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-001122-22"
-  location = "europe-west2"
+  name     = "	trusty-obelisk-412501-22"
+  location = "us-central1"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-random-00112323"
-  location = "europe-west2"
+  name     = "	trusty-obelisk-412501-00112323"
+  location = "us-central1"
 }
